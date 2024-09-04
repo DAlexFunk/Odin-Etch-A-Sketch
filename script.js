1aa2aa96806a26bf2f4e59c76489e3d15c631558
Alex Funk <alexdagreat04@gmail.com>
2024-09-03 20:48:31 -0400

    Added the base board and beginning of button

---
+++ script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const squares = document.querySelectorAll(".square");
+const button = document.querySelector("button");
+setBoxSize(16);
+
+button.addEventListener("click", () => {
+    let boxSize = 0;
+    do {
+        boxSize = prompt("What size do you want the box to be (square) max of 100");
+    } while(boxSize > 100);
+
+    clearChildren(".container");
+    // setBoxSize(boxSize);
+})
+
+
+function setBoxSize (boxSize) {
+    for (let i = 0; i < boxSize; i++) {
+        const row = document.createElement("div");
+        row.setAttribute("class", "row");
+        for (let j = 0; j < boxSize; j++) {
+            const square = document.createElement("div");
+            square.setAttribute("class", "square");
+            square.addEventListener("mouseover", () => square.setAttribute("class", "hoveredSquare"))
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+}
